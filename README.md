cedd84bb53e23c376fd5cb7b08f4f56ec0b8c120
jeigrei <j.g.cupit@gmail.com>
2019-11-03 16:47:02 -0600

    application logic, update TODOs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CMXML-UI
 
 CMXML-UI is a minimal GUI frontend for Coh-MetrixML.
+
+# TODO
+* Fix `txtStdOut` to be scrollable
+* General aesthetic improvements - Use QLayouts to either make the whole app resizable or fix the size of the whole window
+* Use a `QThreadPool` approach with signals slots to allow correct UI updating from reader thread
+* Native error handling to notify user and prevent malformed calls to backend
--- src/App.py
+++ src/App.py
@@ -1,5 +1,9 @@
 import sys
+from threading import Thread
+import subprocess
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtCore import *
+
 from MainWindow import Ui_MainWindow
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -14,8 +18,13 @@ class Main(QMainWindow, Ui_MainWindow):
         self.btnOutputBrowse.clicked.connect(self.btnOutputBrowse_clicked)
         #============================================#
 
+
+    #+++++++++++++++ Event handlers +++++++++++++++#
     def btnAnalyze_clicked(self):
-        pass
+        inputFile, outputFile, language = self.getCLIArgs()
+        proc = subprocess.Popen(['cohmetrix','-i',inputFile,'-o',outputFile,'-l',language,'-n','-8'], stdout=subprocess.PIPE)
+        t = Thread(target=self.stdOutReader, args=[proc])
+        t.start()
 
     def btnInputBrowse_clicked(self):
         inputFile = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
@@ -24,7 +33,17 @@ class Main(QMainWindow, Ui_MainWindow):
     def btnOutputBrowse_clicked(self):
         outputFile, _filter = QFileDialog.getSaveFileName()
         self.lneOutputFile.setText(outputFile)
+    #++++++++++++++++++++++++++++++++++++++++++++++#
+    
 
+    #+++++++++++++++ Helpers ++++++++++++++++++++++#
+    def stdOutReader(self, proc):
+        for line in iter(proc.stdout.readline, b''):
+            self.txtStdOut.append(line.decode('utf-8'))
+
+    def getCLIArgs(self):
+        return (self.lneInputFile.text(), self.lneOutputFile.text(), self.cbLanguage.currentText())
+    #++++++++++++++++++++++++++++++++++++++++++++++#
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

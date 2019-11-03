c6b156dadfe79f3d9b390d7a141bd111c0f2561a
jeigrei <j.g.cupit@gmail.com>
2019-11-03 10:24:41 -0600

    class-based entry point

--- src/App.py
+++ src/App.py
@@ -2,10 +2,16 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from MainWindow import Ui_MainWindow
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
+class Main(QMainWindow, Ui_MainWindow):
 
-window.show()
-sys.exit(app.exec_())
+    def __init__(self, parent= None):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())

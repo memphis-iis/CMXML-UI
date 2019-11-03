ff8caf02fd9a70ad58226b5cf87dc0465a5b2c88
jeigrei <j.g.cupit@gmail.com>
2019-11-03 10:57:00 -0600

    event handlers, file picking working

--- src/App.py
+++ src/App.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from MainWindow import Ui_MainWindow
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -8,6 +8,22 @@ class Main(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        #========== Connect event handlers ==========#
+        self.btnAnalyze.clicked.connect(self.btnAnalyze_clicked)
+        self.btnInputBrowse.clicked.connect(self.btnInputBrowse_clicked)
+        self.btnOutputBrowse.clicked.connect(self.btnOutputBrowse_clicked)
+        #============================================#
+
+    def btnAnalyze_clicked(self):
+        pass
+
+    def btnInputBrowse_clicked(self):
+        inputFile = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.lneInputFile.setText(inputFile)
+
+    def btnOutputBrowse_clicked(self):
+        outputFile, _filter = QFileDialog.getSaveFileName()
+        self.lneOutputFile.setText(outputFile)
 
 
 if __name__ == '__main__':
